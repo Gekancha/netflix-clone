e06fad4ebbf4d8c198788086166605b5bde9d148
Goggags123 <wongsakorngag@gmail.com>
2021-11-02 01:13:18 +0700

    Group rows into div content

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,31 +1,48 @@
-import React from "react";
-import Row from "../components/Row";
-import request from "../request";
-import Banner from "../header/Banner";
-import Login from "../header/Navbar/loginV2";
+import React from 'react'
+import Row from '../components/Row'
+import request from '../request'
+import Banner from '../header/Banner'
+import './HomeScreen.css'
+import Login from '../header/Navbar/loginV2'
 
 function HomeScreen() {
-  return (
-    <div className="homecreen">
-      <Login />
+    return (
+        <div className="home_screen">
+            <Login />
 
-      {/* <Nav/> */}
-      <Banner />
-
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={request.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
-      <Row title="Comedy Movie" fetchUrl={request.fetchComedyMovies} />
-      <Row title="Horror Movie" fetchUrl={request.fetchHorrorMovies} />
-      <Row title="Romance Movie" fetchUrl={request.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
-    </div>
-  );
+            {/* <Nav/> */}
+            <Banner />
+            <div className="content">
+                <Row
+                    title="NETFLIX ORIGINALS"
+                    fetchUrl={request.fetchNetflixOriginals}
+                    isLargeRow={true}
+                />
+                <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+                <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+                <Row
+                    title="Action Movies"
+                    fetchUrl={request.fetchActionMovies}
+                />
+                <Row
+                    title="Comedy Movie"
+                    fetchUrl={request.fetchComedyMovies}
+                />
+                <Row
+                    title="Horror Movie"
+                    fetchUrl={request.fetchHorrorMovies}
+                />
+                <Row
+                    title="Romance Movie"
+                    fetchUrl={request.fetchRomanceMovies}
+                />
+                <Row
+                    title="Documentaries"
+                    fetchUrl={request.fetchDocumentaries}
+                />
+            </div>
+        </div>
+    )
 }
 
-export default HomeScreen;
+export default HomeScreen
