79ffee36ffa19ea4ffa54a09833cb02842f3ec38
Goggags123 <wongsakorngag@gmail.com>
2021-11-03 22:58:38 +0700

    Handles loading image error on movie poster

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -4,40 +4,40 @@ import { useState } from 'react'
 const base_url = 'https://image.tmdb.org/t/p/original/'
 
 function MoviePoster({ movie, isLargeRow, displayLoading }) {
-    const smallMovieRatio = { width: '170px', height: '100px' }
-    const largeMovieRatio = { width: '170px', height: '250px' }
+    const smallMovieRatio = { minWidth: '170px', minHeight: '100px' }
+    const largeMovieRatio = { minWidth: '170px', minHeight: '250px' }
     const posterRatio = isLargeRow ? largeMovieRatio : smallMovieRatio
 
     const [isLoading, setLoading] = useState(true)
+    const [isError, setError] = useState(false)
 
     return (
-        // <div key={key} style={{ display: 'contents' }}>
-            <>
-                {displayLoading && isLoading ? (
-                    <div style={{ paddingRight: '10px' }}>
-                        <Skeleton.Button style={posterRatio} active />
-                    </div>
-                ) : null}
-                <img
-                    className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
-                    style={
-                        isLoading
-                            ? { ...posterRatio, display: 'none' }
-                            : { ...posterRatio }
-                    }
-                    src={`${base_url}${
-                        isLargeRow ? movie.poster_path : movie.backdrop_path
-                    }`}
-                    onLoad={() => {
-                        setLoading(false)
-                    }}
-                    onError={()=>{
-                        setLoading(false)
-                    }}
-                    alt={movie.title}
-                />
-            </>
-        // </div>
+        <>
+            {displayLoading && isLoading ? (
+                <div style={{ paddingRight: '10px' }}>
+                    <Skeleton.Button style={posterRatio} active />
+                </div>
+            ) : null}
+            <img
+                className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
+                style={{
+                    ...posterRatio,
+                    ...(isLoading && { display: 'none' }),
+                    ...(isError && { cursor: 'auto', transform: 'none' }),
+                }}
+                src={`${base_url}${
+                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                onLoad={() => {
+                    setLoading(false)
+                }}
+                onError={() => {
+                    setLoading(false)
+                    setError(true)
+                }}
+                alt={movie.title}
+            />
+        </>
     )
 }
 export default MoviePoster
