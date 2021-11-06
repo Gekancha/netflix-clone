dfaf2b41d824fb702039bbd74d7180509d206b99
Goggags123 <wongsakorngag@gmail.com>
2021-11-06 07:44:08 +0700

    Add method for fetchin movie trailer

--- src/request.js
+++ src/request.js
@@ -11,6 +11,8 @@ const request = {
     fetchRomanceMovies: `${url}discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `${url}discover/movie?api_key=${API_KEY}&with_genres=99`,
     fetchMovieGenres: `${url}genre/movie/list?api_key=${API_KEY}&language=en-US`,
+    fetchMovieTrailer: `${url}movie/{id}/videos?api_key=${API_KEY}&language=en-US`,
+    fetchTvShowTrailer: `${url}tv/{id}/videos?api_key=${API_KEY}&language=en-US`,
 }
 
 export default request
